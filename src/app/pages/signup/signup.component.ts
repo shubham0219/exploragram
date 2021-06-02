3b21659776ba99fc907b2d20376b777367e51a9c
shubham0219 <65565631+shubham0219@users.noreply.github.com>
2021-06-02 15:42:31 +0530

    fix: fix bugs

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,91 +1,95 @@
-import { imageConfig } from './../../../utils/config';
-import { AuthService } from './../../services/auth.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+//services
+import { AuthService } from "src/app/services/auth.service";
 
-//form
-import { NgForm } from '@angular/forms';
+//angular form
+import { NgForm } from "@angular/forms";
 
-//rxjs
-import { finalize } from 'rxjs/operators';
-
+import { finalize } from "rxjs/operators";
 //firebase
-import { AngularFireStorage } from '@angular/fire/storage';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireStorage } from "@angular/fire/storage";
+import { AngularFireDatabase } from "@angular/fire/database";
 
 //browser image resizer
-import { readAndCompressImage } from 'browser-image-resizer';
+import { readAndCompressImage } from "browser-image-resizer";
+import { imageConfig } from "src/utils/config";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css'],
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"],
 })
 export class SignupComponent implements OnInit {
+  picture: string =
+    "https://learnyst.s3.amazonaws.com/assets/schools/2410/resources/images/logo_lco_i3oab.png";
+
   uploadPercent: number = null;
+
   constructor(
-    private toastr: ToastrService,
     private auth: AuthService,
     private router: Router,
+    private db: AngularFireDatabase,
     private storage: AngularFireStorage,
-    private db: AngularFireDatabase
+    private toastr: ToastrService,
   ) {}
 
-  picture: string = 'assets/profileDemo.png';
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   onSubmit(f: NgForm) {
-    //following is called destructuring
     const { email, password, username, country, bio, name } = f.form.value;
-    this.auth
-      .signUp(email, password)
+
+    //further sanitization - do here
+
+    this.auth.signUp(email, password)
       .then((res) => {
         console.log(res);
         const { uid } = res.user;
-        this.db.object(`/users/${uid}`).set({
-          id: uid,
-          name: name,
-          email: email,
-          instaUserName: username,
-          country: country,
-          bio: bio,
-          picture: this.picture,
-        });
+
+        this.db.object(`/users/${uid}`)
+          .set({
+            id: uid,
+            name: name,
+            email: email,
+            instaUserName: username,
+            country: country,
+            bio: bio,
+            picture: this.picture,
+          });
       })
       .then(() => {
-        this.router.navigateByUrl('/');
-        this.toastr.success('SignUp success.');
+        this.router.navigateByUrl("/");
+        this.toastr.success("SignUp Success");
       })
       .catch((err) => {
-        this.toastr.error('SignUp failed.');
-        console.log(err);
+        this.toastr.error("Signup failed");
       });
   }
 
   async uploadFile(event) {
     const file = event.target.files[0];
-    let resizeImage = await readAndCompressImage(file, imageConfig);
-    //const filePath = file.name; //rename the image with TODO:uuid
-    const filePath = file.name;
+
+    let resizedImage = await readAndCompressImage(file, imageConfig);
+
+    const filePath = file.name; // rename the image with TODO: UUID
     const fileRef = this.storage.ref(filePath);
 
-    const task = this.storage.upload(filePath, resizeImage);
+    const task = this.storage.upload(filePath, resizedImage);
 
-    task.percentageChanges().subscribe((percentage) => {
-      this.uploadPercent = percentage;
+    task.percentageChanges().subscribe((percetage) => {
+      this.uploadPercent = percetage;
     });
 
-    //its an observable
-    task
-      .snapshotChanges()
+    task.snapshotChanges()
       .pipe(
         finalize(() => {
           fileRef.getDownloadURL().subscribe((url) => {
             this.picture = url;
-            this.toastr.success('Image upload success.');
+            this.toastr.success("image upload success");
           });
-        })
+        }),
       )
       .subscribe();
   }
